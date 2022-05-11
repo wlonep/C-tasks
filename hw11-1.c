0b8bfd3407f2ccd19a9568fb7ace7f8115389e50
Jiwon Park <98960141+support0911@users.noreply.github.com>
2022-05-11 16:49:40 +0900

    Add files via upload

---
+++ hw11-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#pragma warning(disable:26451)
+
+int main(void)
+{
+	int i, j;
+	int* pi = &i, * pj = &j;
+	*pi = 5;
+	*pj = 10;
+
+	double avg;
+	double* pavg = &avg;
+	*pavg = (double) (i + j) / 2;
+	
+	printf("%d %d %lf\n", *pi, *pj, *pavg);
+	printf("%d %d %lf", i, j, avg);
+}
